523f4eab85c7d196f2e8f7a84b01316124c72984
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-20 13:04:47 -0800

    finished initial design of Login form component and added axios, react-router-dom dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Link, Route } from 'react-router-dom';
+import axios from 'axios';
+
+import UserLogin from './components/UserLogin';
+
+// Initial States
+const initLoginValues = {
+  username: '',
+  password: '',
+  remember: false,
+}
+
+const initLoginErrors = {
+  username: '',
+  password: '', 
+  remember: '',
+}
+
+const initLoginDisabled = true;
 
 function App() {
+  const [loginValues, setLoginValues] = useState(initLoginValues);
+  const [loginErrors, setLoginErrors] = useState(initLoginErrors);
+  const [disabled, setDisabled] = useState(initLoginDisabled);
+
+  const postNewLogin = newLogin => {
+    axios.post('', newLogin)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+      .finally(() => {
+        setLoginValues(initLoginValues);
+      })
+  }
+  
+  const loginSubmit = () => {
+    const newLogin = {
+      username: loginValues.username.trim(),
+      password: loginValues.password,
+      remember: loginValues.remember,
+    }
+    postNewLogin(newLogin);
+  }
+
   return (
     <div className="App">
-
+      <Route path='/login'>
+        <UserLogin values={loginValues} submit={loginSubmit} disabled={disabled} errors={loginErrors}/>
+      </Route>
     </div>
   );
 }
