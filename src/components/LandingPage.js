95d26c12263e4303fdfa613a4a0c0d861d4cbde3
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-21 08:06:26 -0800

    updated landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,7 +14,7 @@ export default function LandingPage() {
       <div className='about-section'>
         <h2>About Potluck Planner</h2>
         <p>Gone are the days of texting back and forth to get your potluck organized. With Potluck Planner, simply create an event, enter any essential details, and allow your guests to pick which dishes they'd like to bring.</p>
-        <ul className='list-group'>
+        <ul className='list-group list-group-flush'>
           <li className='list-group-item text-primary'>Create and manage each potluck event</li>
           <li className='list-group-item text-primary'>Invite friends and share details</li>
           <li className='list-group-item text-primary'>Easily update and manage desired potluck dishes</li>
