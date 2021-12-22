d97827766c8882f1a8f316df222c2edc0ca35425
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-12-22 11:16:55 -0800

    update potluck cards

--- src/components/Potlucks.js
+++ src/components/Potlucks.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axiosWithAuth from "../utils/axiosWithAuth";
+import { Accordion } from "react-bootstrap";
 
 const Potlucks = (props) => {
   const token = localStorage.getItem("token");
@@ -29,18 +30,43 @@ const Potlucks = (props) => {
       <br></br>
       {potlucks.map((p) => {
         return (
+          <div className='potluck-wrapper shadow-lg bg-light'>
+            {/* Potluck details */}
             <div key={p.potluck_id}> 
-            <h3>{p.potluck_name}</h3> <br></br>
-            Location: {p.location} <br></br>
-            {p.date} <br></br>
-            {p.time} <br></br>
-
-            <hr></hr>
-
-           
-
-    
+              <h3>{p.potluck_name}</h3> <br></br>
+              Location: {p.location} <br></br>
+              {p.date} <br></br>
+              {p.time} <br></br>
             </div>
+            <div className='potluck-acc-wrapper'>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Add a Guest</Accordion.Header>
+                  <Accordion.Body>
+                    <form>
+                      <input 
+                        name='guest'
+                        type='text'
+                        placeholder='Guest Name'
+                      />
+                    </form>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Add a Menu Item</Accordion.Header>
+                  <Accordion.Body>
+                    <form>
+                      <input 
+                        name='menuItem'
+                        type='text'
+                        placeholder='Name of Menu Item'
+                      />
+                    </form>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
+          </div>
         );
       })}
     </div>
